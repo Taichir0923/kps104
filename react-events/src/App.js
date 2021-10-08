4471cc68a1fdc2cf425195fb2cd9e1504401f0c7
taichir0923 <crystal19950923@gmail.com>
2021-10-08 17:57:13 +0800

    Update App.js

--- react-events/src/App.js
+++ react-events/src/App.js
@@ -63,9 +63,9 @@ function App() {
       </div>
       <div className='w-3/5 mx-auto'>
         <div className='w-full flex gap-2 border-b-2 border-blue-500'>
-          <button className={`py-3 bg-gray-300 rounded-t-xl font-bold px-5` + ` ${atsonh ? 'bg-blue-500 text-white' : 'text-gray-600'}`} onClick={openA}>Бүгд</button>
-          <button className={`py-3 bg-gray-300 rounded-t-xl font-bold px-5` + ` ${btsonh ? 'bg-blue-500 text-white' : 'text-gray-600'}`} onClick={openB}>Хийсэн</button>
-          <button className={`py-3 bg-gray-300 rounded-t-xl font-bold px-5` + ` ${ctsonh ? 'bg-blue-500 text-white' : 'text-gray-600'}`} onClick={openC}>Хийгээгүй</button>
+          <button className={`py-3 bg-gray-300 rounded-t-xl font-bold px-5 ${atsonh ? 'bg-blue-500 text-white' : 'text-gray-600'}`} onClick={openA}>Бүгд</button>
+          <button className={`py-3 bg-gray-300 rounded-t-xl font-bold px-5 ${btsonh ? 'bg-blue-500 text-white' : 'text-gray-600'}`} onClick={openB}>Хийсэн</button>
+          <button className={`py-3 bg-gray-300 rounded-t-xl font-bold px-5 ${ctsonh ? 'bg-blue-500 text-white' : 'text-gray-600'}`} onClick={openC}>Хийгээгүй</button>
         </div>
 
         {
@@ -74,9 +74,9 @@ function App() {
             {
               taskList.map(list => (
                 <li key={`task_${list.id}`} className='flex hover:bg-gray-200 py-2 px-4 rounded-xl justify-between items-center'>
-                  <p className={`font-bold text-gray-600 ` + `${list.completed ? 'line-through' : ''}`}>{list.taskName}</p>
+                  <p className={`font-bold text-gray-600 ${list.completed ? 'line-through' : ''}`}>{list.taskName}</p>
                   <div className='flex items-center gap-2'>
-                    <button onClick={() => statusHandler(list.id)} className={`w-6 h-6 rounded-full flex items-center text-base bg-opacity-50 border-2 hover:bg-opacity-1 justify-center ` + `${list.completed ? 'text-green-400 border-green-400 bg-green-400' : 'text-gray-400 border-gray-400 bg-gray-400'}`}>
+                    <button onClick={() => statusHandler(list.id)} className={`w-6 h-6 rounded-full flex items-center text-base bg-opacity-50 border-2 hover:bg-opacity-1 justify-center ${list.completed ? 'text-green-400 border-green-400 bg-green-400' : 'text-gray-400 border-gray-400 bg-gray-400'}`}>
                       <BsCheck />
                     </button>
                     <button onClick={() => deleteHandler(list.id)} className='w-6 h-6 rounded-full flex items-center text-xs bg-opacity-50 text-red-400 border-2 border-red-400 hover:bg-opacity-100 hover:text-white justify-center bg-red-400'>
@@ -91,16 +91,44 @@ function App() {
 
         {
           btsonh &&
-          <div>
-            Хийсэн
-          </div>
+          <ul className='w-full flex flex-col gap-4 my-5'>
+            {
+              taskList.filter(list => list.completed).map(list => (
+                <li key={`task_${list.id}`} className='flex hover:bg-gray-200 py-2 px-4 rounded-xl justify-between items-center'>
+                  <p className={`font-bold text-gray-600 ${list.completed ? 'line-through' : ''}`}>{list.taskName}</p>
+                  <div className='flex items-center gap-2'>
+                    <button onClick={() => statusHandler(list.id)} className={`w-6 h-6 rounded-full flex items-center text-base bg-opacity-50 border-2 hover:bg-opacity-1 justify-center ${list.completed ? 'text-green-400 border-green-400 bg-green-400' : 'text-gray-400 border-gray-400 bg-gray-400'}`}>
+                      <BsCheck />
+                    </button>
+                    <button onClick={() => deleteHandler(list.id)} className='w-6 h-6 rounded-full flex items-center text-xs bg-opacity-50 text-red-400 border-2 border-red-400 hover:bg-opacity-100 hover:text-white justify-center bg-red-400'>
+                      <FaTimes />
+                    </button>
+                  </div>
+                </li>
+              ))
+            }
+          </ul>
         }
 
         {
           ctsonh &&
-          <div>
-            Хийгээгүй
-          </div>
+          <ul className='w-full flex flex-col gap-4 my-5'>
+            {
+              taskList.filter(list => !list.completed).map(list => (
+                <li key={`task_${list.id}`} className='flex hover:bg-gray-200 py-2 px-4 rounded-xl justify-between items-center'>
+                  <p className={`font-bold text-gray-600 ${list.completed ? 'line-through' : ''}`}>{list.taskName}</p>
+                  <div className='flex items-center gap-2'>
+                    <button onClick={() => statusHandler(list.id)} className={`w-6 h-6 rounded-full flex items-center text-base bg-opacity-50 border-2 hover:bg-opacity-1 justify-center ${list.completed ? 'text-green-400 border-green-400 bg-green-400' : 'text-gray-400 border-gray-400 bg-gray-400'}`}>
+                      <BsCheck />
+                    </button>
+                    <button onClick={() => deleteHandler(list.id)} className='w-6 h-6 rounded-full flex items-center text-xs bg-opacity-50 text-red-400 border-2 border-red-400 hover:bg-opacity-100 hover:text-white justify-center bg-red-400'>
+                      <FaTimes />
+                    </button>
+                  </div>
+                </li>
+              ))
+            }
+          </ul>
         }
       </div>
     </>
